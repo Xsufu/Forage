68474074d0c6d538cf8a164d0ce8bae878050f7d
Xsufu <aluno.macarrao@gmail.com>
2022-06-10 18:20:02 +0700

    Рефакторинг

--- app/src/main/java/com/example/forage/model/Forageable.kt
+++ app/src/main/java/com/example/forage/model/Forageable.kt
@@ -28,8 +28,6 @@ data class Forageable(
     val id: Long = 0,
     val name: String,
     val address: String,
-    // TODO: make a custom column name for the inSeason variable that follows SQL column name
-    //  conventions (the column name should be in_season)
     @ColumnInfo(name = "in_season")
     val inSeason: Boolean,
     val notes: String?
